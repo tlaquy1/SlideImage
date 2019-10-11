7dee4f2a4b5a76919a7d73791882bbfd2cd3f8e5
tlaquy1 <phamdat.huhu1997@gmail.com>
2019-10-11 21:31:12 +0700

    fix

---
+++ SL/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnS = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(30, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(479, 280);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(209, 321);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(105, 56);
+            this.btnS.TabIndex = 2;
+            this.btnS.Text = "Start";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.BtnS_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 389);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ SL/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SL
+{
+   
+    public partial class Form1 : Form
+    {
+        string[] files;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            files = Directory.GetFiles(Application.StartupPath + @"\AnhSlide");
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnS_Click(object sender, EventArgs e)
+        {
+            if (!timer.Enabled)
+            {
+                timer.Start();
+                btnS.Text = "Stop";
+            }
+            else
+            {
+                timer.Stop();
+                btnS.Text = "Start";
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (i == files.Length)
+            {
+                timer.Stop();
+                i = 0;
+                btnS.Text = "Start";
+            }
+            else
+            {
+                FileInfo file = new FileInfo(files[i]);
+
+                var anhDaiDien = Image.FromFile(Application.StartupPath + @"\AnhSlide\" + file.Name);
+                pictureBox.Image = anhDaiDien;
+
+                i++;
+            }
+        }
+    }
+}
